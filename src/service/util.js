168f65f087d337dcc075941c9e8f692a2a586c2a
HarryBlackCatQAQ <641779160@qq.com>
2019-10-01 03:47:46 +0800

    1.将Login.vue的登录div 组件化 2.新增Login页面的组件 loginModel forgetPasswordModel registeredModel  (forgetPasswordModel 和 registeredModel 未完成) 3.util.js新增 sleep方法

--- src/service/util.js
+++ src/service/util.js
@@ -2,7 +2,7 @@
  * @Author: Harry 
  * @Date: 2019-09-18 03:39:07 
  * @Last Modified by: Harry-mac
- * @Last Modified time: 2019-09-20 01:46:30
+ * @Last Modified time: 2019-10-01 03:42:02
  */
 import sha1 from "sha1";
 
@@ -39,8 +39,16 @@ export default {
      * 使用 SHA1算法进行加密
      * @param {string} password 
      */
-    SHA1(password){
+    SHA1(password) {
         return sha1(password);
+    },
+
+    /**
+     *  sleep睡眠工具 time 1000为1秒
+     * @param {*} time 
+     */
+    sleep(time) {
+        return new Promise(resolve => setTimeout(resolve, time));
     }
 }
 
@@ -48,11 +56,11 @@ export default {
  * 判断是否有html后缀，没有自动帮其加上
  * @param {string} path 
  */
-function isHtml(path){
+function isHtml(path) {
     let idx = path.indexOf(".");
-    if(idx == -1){
-      return path + ".html";
+    if (idx == -1) {
+        return path + ".html";
     }
-  
+
     return path;
-  }
+}
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -2,90 +2,118 @@
  * @Author: Harry 
  * @Date: 2019-10-01 02:48:06 
  * @Last Modified by: Harry-mac
- * @Last Modified time: 2019-10-01 02:55:45
+ * @Last Modified time: 2019-10-01 03:45:30
  */
 
 <template>
   <div class="login-view">
     Login页面
+    <el-button @click="changeView('login')">Click Me</el-button>
     <el-card class="login-card" type="flex">
-      <!-- <div v-for="o in 4" :key="o" class="text item">
-          {{'列表内容 ' + o }}
+      <transition name="el-zoom-in-center">
+        <login-model v-show="loginModel_show" v-on:changeView="changeView" />
+      </transition>
+
+      <transition name="el-zoom-in-center">
+        <forgetPassword-model v-show="forgetPasswordModel_show" v-on:changeView="changeView" />
+      </transition>
+
+      <transition name="el-zoom-in-center">
+        <registered-model v-show="registeredModel_show" v-on:changeView="changeView" />
+      </transition>
+
+      <!-- <div class="login-model">
+        <div class="input-group">
+          <el-row :gutter="20" type="flex" justify="center">
+            <el-col :span="2">
+              <i class="el-icon-user icon"></i>
+            </el-col>
+            <el-col :span="22">
+              <el-input placeholder="账号" v-model="username" clearable></el-input>
+            </el-col>
+          </el-row>
+          <el-row :gutter="20">
+            <el-col :span="2">
+              <i class="el-icon-lock icon"></i>
+            </el-col>
+            <el-col :span="22">
+              <el-input placeholder="密码" type="password" v-model="password" clearable></el-input>
+            </el-col>
+          </el-row>
+        </div>
+
+        <div class="remember-me">
+          <el-row type="flex">
+            <el-col :span="4" class="remember-me-select">
+              <el-checkbox v-model="rememberMe">记住我</el-checkbox>
+            </el-col>
+
+            <el-col :span="8" class="remember-link">
+              <el-link type="primary">忘记密码</el-link>
+            </el-col>
+            <el-col :span="8">
+              <el-link type="primary">立即注册</el-link>
+            </el-col>
+          </el-row>
+        </div>
+
+        <div class="button-group">
+          <el-row type="flex">
+            <el-col :span="8">
+              <el-button class="login-button" type="primary" @click="login">登录</el-button>
+            </el-col>
+          </el-row>
+        </div>
       </div>-->
-      <div class="input-group">
-        <el-row :gutter="20" type="flex" justify="center">
-          <el-col :span="2">
-            <i class="el-icon-user icon"></i>
-          </el-col>
-          <el-col :span="22">
-            <el-input placeholder="账号" v-model="username" clearable></el-input>
-          </el-col>
-        </el-row>
-        <el-row :gutter="20">
-          <el-col :span="2">
-            <i class="el-icon-lock icon"></i>
-          </el-col>
-          <el-col :span="22">
-            <el-input placeholder="密码" type="password" v-model="password" clearable></el-input>
-          </el-col>
-        </el-row>
-      </div>
-
-      <div class="remember-me">
-        <el-row type="flex">
-          <el-col :span="4" class="remember-me-select">
-            <el-checkbox v-model="rememberMe">记住我</el-checkbox>
-          </el-col>
-
-          <el-col :span="8" class="remember-link">
-            <el-link type="primary">忘记密码</el-link>
-          </el-col>
-          <el-col :span="8">
-            <el-link type="primary">立即注册</el-link>
-          </el-col>
-        </el-row>
-      </div>
-
-      <div class="button-group">
-        <el-row type="flex">
-          <el-col :span="8">
-            <el-button class="login-button" type="primary" @click="login">登录</el-button>
-          </el-col>
-        </el-row>
-      </div>
     </el-card>
   </div>
 </template>
 
 <script>
-import loginService from "@/service/loginService";
-import routerApi from '@/service/api/routerApi'
+import loginModel from "@/components/login/loginModel";
+import forgetPasswordModel from "@/components/login/forgetPasswordModel";
+import registeredModel from "@/components/login/registeredModel";
+import util from "@/service/util";
 
 export default {
   name: "login",
+  components: {
+    loginModel,
+    forgetPasswordModel,
+    registeredModel
+  },
   data() {
     return {
-      username: "",
-      password: "",
-      rememberMe: false
+      loginModel_show: true,
+      forgetPasswordModel_show: false,
+      registeredModel_show: false,
+      changTime: 200
     };
   },
   methods: {
-    login() {
-      let res = loginService.login(this.username, this.password);
+    changeView(val) {
+      if (val == "registered") {
+        this.loginModel_show = false;
+        this.forgetPasswordModel_show = false;
 
-      res.then(response => {
-        console.log(response);
-        if(response.flag){
-            this.$router.push(routerApi.home)
-        }
-        else {
-          this.$message({
-            message: "用户或者密码错误！",
-            type: "error"
-          });
-        }
-      });
+        util.sleep(this.changTime).then(() => {
+          this.registeredModel_show = true;
+        });
+      } else if (val == "forgetPassword") {
+        this.loginModel_show = false;
+        this.registeredModel_show = false;
+
+        util.sleep(this.changTime).then(() => {
+          this.forgetPasswordModel_show = true;
+        });
+      } else {
+        this.forgetPasswordModel_show = false;
+        this.registeredModel_show = false;
+
+        util.sleep(this.changTime).then(() => {
+          this.loginModel_show = true;
+        });
+      }
     }
   }
 };
@@ -105,35 +133,5 @@ export default {
   width: 350px;
   height: 500px;
   margin: 5% auto;
-}
-
-.input-group {
-  margin-top: 20%;
-}
-
-.el-row {
-  margin-bottom: 15%;
-}
-
-.icon {
-  font-size: 20px;
-  margin-top: 10px;
-}
-.remember-me {
-  margin-left: 2px;
-}
-
-.remember-me-select {
-  margin-top: 1px;
-}
-
-.remember-link {
-  margin-left: 60px;
-  padding-bottom: 20px;
-}
-
-.login-button {
-  width: 250px;
-  margin-left: 30%;
 }
 </style>

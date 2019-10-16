9168f65addf60b412608d37a5119414ea817cd7b
HarryBlackCatQAQ <641779160@qq.com>
2019-10-16 12:15:49 +0800

    no

--- src/service/propertyService.js
+++ src/service/propertyService.js
@@ -1,61 +1,24 @@
 /*
  * @Author: Hovees
  * @Date: 2019-10-08 16:23:50
- * @Last Modified by: Hovees-hwx
- * @Last Modified time: 2019-10-10 17:03:14
+ * @Last Modified by: Harry-mac
+ * @Last Modified time: 2019-10-16 12:13:54
  */
 
 import _this from '@/main'
 import api from '@/service/api/serviceApi'
+import propertyModule from '@/store/modules/propertyModule'
 
 const that = _this._this
 
 export default {
   async getProperty() {
     let res
-    let pageNum = that.$store.state.property.pageNum
-    let pageSize = that.$store.state.property.pageSize
+    let pageNum = propertyModule.state.pageNum
+    let pageSize = propertyModule.state.pageSize
     await that.$get(api.property.url.findAll, {
       pageNum: pageNum,
       pageSize: pageSize
-    })
-    .then(response => {
-      res = response
-      if(res.message === '查询成功') {
-        that.$store.commit('property/GET_PROPERTY', response.data)
-      }
-    })
-    return res
-  },
-  async checkPropertyName(propertyName) {
-    let res
-    await that.$get(api.property.url.checkPropertyName, {
-      propertyName: propertyName
-    })
-    .then(response => {
-      res = response
-    })
-    return res
-  },
-  async insert(addPropertyModel) {
-    let res
-    await that.$post(api.property.url.insert, {
-      name: addPropertyModel.name,
-      location: addPropertyModel.location,
-      uid: addPropertyModel.uid
-    })
-    .then(response => {
-      res = response
-    })
-    return res
-  },
-  async update(recodeProperty) {
-    let res
-    await that.$patch(api.property.url.update, {
-      id: recodeProperty.id,
-      name: recodeProperty.name,
-      location: recodeProperty.location,
-      uid: recodeProperty.uid,
     })
     .then(response => {
       res = response
